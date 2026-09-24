4bd62f01ddec348e29a4cafba3e62599f1a26c26
SRI <srivardhinibalaji2004@gmail.com>
2026-09-25 00:46:55 +0530

    Folder structure created

---
+++ template.sh
@@ -0,0 +1,15 @@
+# Creating Directories
+mkdir -p src
+mkdir -p resources
+
+# Creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch app.py
+touch setup.py
+touch resources/trials.ipynb
+touch requirements.txt
+
+echo "Directory & files created successfully."
